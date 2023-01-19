9d8f333074a0a6befc230a76d9b5163a0b600a14
Sina <alisinai850@gmail.com>
2023-01-19 17:05:43 +0330

    added properties and constructor

---
+++ App.Domain.Core/Operators/Entities/Operator.cs
@@ -0,0 +1,28 @@
+﻿using App.Domain.Core.Entities.Users;
+using App.Domain.Core.Operators.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Operators.Entities
+{
+    internal class Operator : User
+    {
+        public Operator (int userId, int operatorId, string username, string password, string firstname, string lastname, int mobile)
+            :base(userId, username, password)
+        {
+            OperatorID = operatorId;
+
+        }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int OperatorID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Mobile { get; set; }
+        public OperatorLevelEnum OperatiorLevel { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ App.Domain.Core/Operators/Enums/OperatorLevelEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Operators.Enums
+{
+    enum OperatorLevelEnum
+    {
+        teacher = 1,
+        admin
+    }
+}
